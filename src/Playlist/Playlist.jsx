f5ef4ade20d2d46206924b7156e32958e35facc4
Melissa Kuris <melissakuris@hotmail.com>
2025-01-29 15:39:27 -0300

    Added Styles

--- src/Playlist/Playlist.jsx
+++ src/Playlist/Playlist.jsx
@@ -13,7 +13,7 @@ function Playlist(props) {
     return (
         <div className={styles.container}>
             <form onSubmit={e => e.preventDefault()}>
-                <input type="text" value={playlistTitle} onChange={({target}) => setPlaylistTitle(target.value)} />
+                <input className={styles.inputTitle} type="text" placeholder="Unnamed Playlist" value={playlistTitle} onChange={({target}) => setPlaylistTitle(target.value)} />
             </form>
             {props.playlist.length > 0 && (
             <Tracklist icon="-" trackArray={props.playlist} onTrackClick={handleTrackClick} />
--- src/SearchBar/SearchBar.jsx
+++ src/SearchBar/SearchBar.jsx
@@ -9,6 +9,7 @@ function SearchBar() {
             <form className={styles.searchBarForm} onSubmit={(e) => e.preventDefault()}>
                 <input type="text" placeholder="Search a song" className={styles.userInput} value={input} onChange={({target}) => setInput(target.value)}  />
             </form>
+            <button className={styles.searchButton}>SEARCH</button>
         </div>
     )
 }
