5342f8caa7ed6345cff8d96d84b2480c23889b8d
Melissa Kuris <melissakuris@hotmail.com>
2025-02-05 12:51:42 -0300

    Updated Track Styles

--- src/Track/Track.jsx
+++ src/Track/Track.jsx
@@ -3,6 +3,7 @@ import styles from "./Track.module.css";
 
 function Track(props) {
     const { track: { name, artist, album, uri }, icon } = props;
+    const trackInfo = `${artist} | ${album}`;
 
 
     const handleClick = () => {
@@ -15,9 +16,8 @@ function Track(props) {
         <div className={styles.track}>
             <div className={styles.trackData}>
                 <p className={styles.trackTitle}>{name}</p>
-                <div className={styles.trackInfo}>
-                    <p className={styles.trackArtist}>{artist}</p>
-                    <p className={styles.trackAlbum}>{album}</p>
+                <div className={styles.trackInfoContainer}>
+                    <p className={styles.trackInfo}>{trackInfo}</p>
                 </div>
             </div>
             <div className={styles.trackIcon}>
