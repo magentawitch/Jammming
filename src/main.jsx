3310ce218ac851983d3e6579cd069258eb3eaa58
Melissa Kuris <melissakuris@hotmail.com>
2025-02-06 13:03:16 -0300

    Responsiveness added
    breakpoint for html fontsize

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import "./reset.css"
+import "./reset.css";
+import './index.css';
 import AuthWrapper from './Auth/AuthWrapper.jsx'
 
 createRoot(document.getElementById('root')).render(
