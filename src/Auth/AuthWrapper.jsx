afb00623ec21e836fce2c21ef0e3273b1bed5017
Melissa Kuris <melissakuris@hotmail.com>
2025-05-06 10:11:30 -0300

    Update AuthWrapper.jsx

--- src/Auth/AuthWrapper.jsx
+++ src/Auth/AuthWrapper.jsx
@@ -40,47 +40,4 @@ function AuthWrapper() {
 
 }
 
-
-/* 
-function AuthWrapper() {
-    const [accessToken, setAccessToken] = useState(localStorage.getItem("access_token"))
-
-    useEffect(() => {
-        const checkToken = async () => {
-
-            const params = new URLSearchParams(window.location.search);
-            const code = params.get("code");
-
-            if (!accessToken && !code) {
-                loginWithSpotify();
-            }
-
-            if (code && !accessToken) {
-                const newToken = await getToken(code);
-                if (newToken) setAccessToken(newToken);
-            }
-
-            if (accessToken) {
-                const validToken = await getValidAccessToken();
-                if (validToken) setAccessToken(validToken)
-
-            }
-
-        }
-
-        checkToken();
-    }, [accessToken]);
-
-    if (accessToken) {
-        return (
-            <App token={accessToken} />
-        )
-    } else {
-        return (
-            <p>Llora</p>
-        )
-    }
-
-} */
-
 export default AuthWrapper;
